69d02799a7f876448a0d9c2545ffb6b93a697671
finny <phineas.ehrlich2026@gbwl.org>
2026-03-10 19:07:48 +0000

    added sql

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("hello world")
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db =  SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+@app.route('/', methods=['GET', 'POST',])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Task(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error adding task'
+        
+    all_tasks = Task.query.all()
+
+if __name__ == '__main__':
+    with app.app.context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
